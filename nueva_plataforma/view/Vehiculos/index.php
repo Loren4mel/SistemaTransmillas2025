a27b7ef2f8d7bdce6758513a5b06a5012a69bc36
jarmandoR <jose523a@gmail.com>
2026-06-02 18:27:22 -0500

    wip: se actualizan cambios de modulo vehiculos

--- nueva_plataforma/view/Vehiculos/index.php
+++ nueva_plataforma/view/Vehiculos/index.php
@@ -13,12 +13,8 @@
 <!-- <link rel="stylesheet" href="/SistemaTransmillas2025/nueva_plataforma/assets/css/vehiculos.css">
 <link rel="shortcut icon" href="/SistemaTransmillas2025/images/Logo Google Nuevo.png"> -->
 <!-- Produccion -->
-<!-- Ruta base de la aplicación (calculada por el servidor) -->
-    <script>
-        window.APP_BASE_URL = "<?= $appBasePath ?>";
-    </script>
-    <link rel="stylesheet" href="<?= $appBasePath ?>/assets/css/vehiculos.css">
-<link rel="shortcut icon" href="<?= $appBasePath ?>/../images/Logo Google Nuevo.png">
+<link rel="stylesheet" href="../assets/css/vehiculos.css">
+<link rel="shortcut icon" href="../../images/Logo Google Nuevo.png">
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
@@ -1241,6 +1237,6 @@
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <!-- <script src="/SistemaTransmillas2025/nueva_plataforma/assets/js/vehiculos.js"></script> -->
-<script src="<?= $appBasePath ?>/assets/js/vehiculos.js"></script>
+<script src="../assets/js/vehiculos.js"></script>
 </body>
 </html>
